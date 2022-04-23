a70e4610eab229e41beab91e2c0627a14c4b2a30
root <root@localhost>
2022-04-23 10:32:30 +0600

    auto publish 1650688349924

--- src/for_loop.py
+++ src/for_loop.py
@@ -2,3 +2,15 @@ nums = [12, 54, -21, 113]
 for num in nums:
    if num > 0:
      print(num)
+
+
+nums = [12, 54, -21, 113, 35]
+for num in nums:
+   if num < 0:
+     break
+   print(num)
+
+     
+
+
+
