6fb92da5c78e627a3321a836ca1ab62bdf7a33f6
root <root@localhost>
2022-04-23 22:26:40 +0600

    auto publish 1650731200672

--- src/adv_func.py
+++ src/adv_func.py
@@ -60,3 +60,74 @@ things = get_a_lot(5)
 
 
 
+# Define `main()` function
+def main():
+ hello()
+ print("This is the main function")
+
+
+# Execute `main()` function
+if __name__ == '__main__':
+   main()
+
+
+
+
+def add_everything(*args):
+    return sum(args)
+ 
+# Calculate the sum
+print(add_everything(1,4,5, 75, 112))
+
+
+
+
+
+
+
+c = 5
+def add (a, b):
+    c = a + b
+    return c
+ 
+# will get an exception
+x = add(12, 23)
+print(x)
+ 
+# use global
+ 
+def add (a, b):
+    global c
+    c = a + b
+    return c
+def add (a, b):
+    return a + b
+    
+    
+    
+ 
+x = add(a = 2, b =3)
+print(x)
+ 
+y = add(b = 7, a=11)
+print(y)
+ 
+total = add(b=x,a=y)
+print(total)
+
+
+
+
+
+double = lambda x: x * 2
+# Output: 10
+print(double(5))
+
+
+
+
+
+
+
+
+
