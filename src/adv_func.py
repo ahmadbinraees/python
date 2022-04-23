c97cade3cfc878c1d36efe444595f76896184318
root <root@localhost>
2022-04-23 22:16:28 +0600

    auto publish 1650730588808

--- src/adv_func.py
+++ src/adv_func.py
@@ -10,3 +10,31 @@ print(y)
 total = add(x,y)
 print(total)
 
+
+
+
+def add (a, b):
+    return a + b
+ 
+x = add(a = 2, b =3)
+print(x)
+ 
+y = add(b = 7, a=11)
+print(y)
+ 
+total = add(b=x,a=y)
+print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
